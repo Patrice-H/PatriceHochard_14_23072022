beb7a58211875b9cd85b3b58dcd3730c41bdd78e
Patrice-H <webmestre64@gmail.com>
2022-07-26 14:03:36 +0200

    Defining Router Logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Employees from './pages/Employees';
+import Home from './pages/Home';
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employees" element={<Employees />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
