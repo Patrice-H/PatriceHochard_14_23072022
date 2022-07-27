b7902f84abd874f06273ed5e21aa62f249c405e5
Patrice-H <webmestre64@gmail.com>
2022-07-27 16:45:01 +0200

    Adding options list for drop down component

---
+++ src/data/departmentsList.js
@@ -0,0 +1,7 @@
+export const departmentsList = [
+  { value: 'sales', label: 'Sales' },
+  { value: 'marketing', label: 'Marketing' },
+  { value: 'engineering', label: 'Engineering' },
+  { value: 'human-resources', label: 'Human Resources' },
+  { value: 'legal', label: 'Legal' },
+];
