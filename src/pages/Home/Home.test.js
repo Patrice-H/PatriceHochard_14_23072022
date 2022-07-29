e2ea7b46553a5aef5cf7df7ba735c71e0aa0cb87
Patrice-H <webmestre64@gmail.com>
2022-07-29 16:14:05 +0200

    Adding modal tests

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { useAppDispatch } from '../../utils/hooks';
 import Home from './index';
@@ -52,5 +52,32 @@ describe('Home page test suites', () => {
     );
     const expectedSubtitle = screen.getByText('Address');
     expect(expectedSubtitle).toBeInTheDocument();
+  });
+
+  it('Should open a modal when user add an employee', () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>
+    );
+    const modal = screen.getByTestId('modal');
+    const button = screen.getByText('Save');
+    expect(modal.classList.contains('hidden')).toEqual(true);
+    fireEvent.click(button);
+    expect(modal.classList.contains('hidden')).toEqual(false);
+  });
+
+  it('Should close the modal when user click on close button', () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>
+    );
+    const modal = screen.getByTestId('modal');
+    const openButton = screen.getByText('Save');
+    fireEvent.click(openButton);
+    const closeButton = screen.getByTestId('close-modal-btn');
+    fireEvent.click(closeButton);
+    expect(modal.classList.contains('hidden')).toEqual(true);
   });
 });
