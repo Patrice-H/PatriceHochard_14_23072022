3094d5250f500d4d3eee556af87dff422217ae19
Patrice-H <webmestre64@gmail.com>
2022-08-02 12:18:02 +0200

    Embeded the app with persistgate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { persistStore } from 'reduxjs-toolkit-persist';
+import { PersistGate } from 'reduxjs-toolkit-persist/integration/react';
 import { store } from './redux/store';
 import App from './App';
 import './index.css';
+
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
