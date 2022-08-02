6b4639e1f63d3585f179a3423904d05ca8675818
Patrice-H <webmestre64@gmail.com>
2022-08-02 12:13:47 +0200

    Updated store with a persisted reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,27 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'reduxjs-toolkit-persist';
+import storage from 'reduxjs-toolkit-persist/lib/storage';
+import autoMergeLevel1 from 'reduxjs-toolkit-persist/lib/stateReconciler/autoMergeLevel1';
 import employeesReducer from './employeesSlice';
 import displayOptionsReducer from './displayOptionsSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel1,
+};
 
 const appReducers = combineReducers({
   employees: employeesReducer,
   displayOptions: displayOptionsReducer,
 });
 
+const _persistedReducer = persistReducer(persistConfig, appReducers);
+
 export const store = configureStore({
-  reducer: appReducers,
+  reducer: _persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
