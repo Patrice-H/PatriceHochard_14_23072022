d33e855c8c4991487ea66dcd945dff6929fc3bed
Patrice-H <webmestre64@gmail.com>
2022-08-03 20:05:39 +0200

    Added a section component with entries informations and page manager

---
+++ src/components/BottomTableDisplayOptions/index.jsx
@@ -0,0 +1,14 @@
+import TableEntriesInformations from '../TableEntriesInformations';
+import TablePageManager from '../TablePageManager';
+import './BottomTableDisplayOptions.css';
+
+const BottomTableDisplayOptions = () => {
+  return (
+    <div id="bottom-table-display-options">
+      <TableEntriesInformations />
+      <TablePageManager />
+    </div>
+  );
+};
+
+export default BottomTableDisplayOptions;
