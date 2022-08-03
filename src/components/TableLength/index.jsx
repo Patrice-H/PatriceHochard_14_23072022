67f7d66f24d3c819da600dbd0e98d89cbb0c5ad6
Patrice-H <webmestre64@gmail.com>
2022-08-03 08:52:58 +0200

    Added a functionality to define table length

--- src/components/TableLength/index.jsx
+++ src/components/TableLength/index.jsx
@@ -1,13 +1,25 @@
+import { useAppDispatch } from '../../utils/hooks';
+import { setTableLength } from '../../redux/displayOptionsSlice';
 import './TableLength.css';
 
 const TableLength = () => {
   const options = [10, 25, 50, 100];
+  const dispatch = useAppDispatch();
+
+  const defineTableLength = () => {
+    const selectValue = document.getElementById('select-table-length').value;
+    dispatch(setTableLength(selectValue));
+  };
 
   return (
     <div id="table-length" data-testid="table-length">
       <label>
         Show
-        <select data-testid="select-table-length">
+        <select
+          id="select-table-length"
+          data-testid="select-table-length"
+          onChange={defineTableLength}
+        >
           {options.map((option) => (
             <option key={option} value={option}>
               {option}
