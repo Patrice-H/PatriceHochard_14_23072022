93e6434aad48911fbf129164e8b435242f7eb3bf
Patrice-H <webmestre64@gmail.com>
2022-08-08 16:02:28 +0200

    Added a mocked store to test components

--- src/utils/testsUtils.js
+++ src/utils/testsUtils.js
@@ -1,13 +1,59 @@
+import { configureStore, combineReducers, createSlice } from '@reduxjs/toolkit';
+import { employeesList } from '../data/dataTest';
+
 const state = {
   employees: {
-    list: [],
+    list: employeesList,
   },
   displayOptions: {
     sortBy: '',
     orderBy: '',
-    tableLength: 10,
+    tableLength: 2,
     pageNumber: 1,
   },
 };
+
+/* istanbul ignore next */
+const employeesSlice = createSlice({
+  name: 'employees',
+  initialState: state.employees,
+  reducers: {
+    addEmployee: (state, action) => {
+      state.list = [...state.list, action.payload];
+    },
+  },
+});
+
+/* istanbul ignore next */
+const displayOptionsSlice = createSlice({
+  name: 'display-options',
+  initialState: state.displayOptions,
+  reducers: {
+    setSortBy: (state, action) => {
+      state.sortBy = action.payload;
+    },
+    setOrderBy: (state, action) => {
+      state.orderBy = action.payload;
+    },
+    setTableLength: (state, action) => {
+      state.tableLength = action.payload;
+    },
+    setPageNumber: (state, action) => {
+      state.pageNumber = action.payload;
+    },
+    setSearchFilter: (state, action) => {
+      state.searchFilter = action.payload;
+    },
+  },
+});
+
+const appReducers = combineReducers({
+  employees: employeesSlice.reducer,
+  displayOptions: displayOptionsSlice.reducer,
+});
+
+export const mockedStore = configureStore({
+  reducer: appReducers,
+});
 
 export const mockedAppSelector = (f) => f(state);
