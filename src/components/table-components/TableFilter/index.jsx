f37502f365e6421324a32c9ad79cdfda7870c2d7
Patrice-H <webmestre64@gmail.com>
2022-08-08 16:03:17 +0200

    Updated component

--- src/components/table-components/TableFilter/index.jsx
+++ src/components/table-components/TableFilter/index.jsx
@@ -1,10 +1,27 @@
-import InputBlock from '../../form-components/InputBlock';
+import { useAppSelector, useAppDispatch } from '../../../utils/hooks';
+import { setSearchFilter } from '../../../redux/displayOptionsSlice';
 import './TableFilter.css';
 
 const TableFilter = () => {
+  const dispatch = useAppDispatch();
+  const searchedText = useAppSelector(
+    (state) => state.displayOptions.searchFilter
+  );
+
+  const searchText = () => {
+    const text = document.getElementById('search-filter').value;
+    if (text.length >= 3) {
+      dispatch(setSearchFilter(text));
+    }
+    if (text.length < 3 && searchedText !== null) {
+      dispatch(setSearchFilter(null));
+    }
+  };
+
   return (
     <div id="table-filter" data-testid="table-filter">
-      <InputBlock labelContent="Search:" elementType="text" />
+      <label htmlFor="search-filter">Search:</label>
+      <input type="text" id="search-filter" onChange={searchText} />
     </div>
   );
 };
