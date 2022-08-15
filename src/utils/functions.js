c36e7e3d065b327bbde3c4507e0fdb49e4933c03
Patrice-H <webmestre64@gmail.com>
2022-08-16 00:35:48 +0200

    Outsourcing functions

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -121,3 +121,51 @@ export const formatDate = (date) => {
 
   return `${year}-${month}-${day}`;
 };
+
+export const formatId = (labelContent) => {
+  const regex = / /g;
+  const id = labelContent.replace(regex, '-').toLowerCase();
+
+  return id;
+};
+
+export const formatName = (labelContent) => {
+  const tabName = labelContent.split(' ');
+  let name = tabName[0].toLowerCase();
+  for (let i = 1; i < tabName.length; i++) {
+    name = name + tabName[i].charAt(0).toUpperCase() + tabName[i].slice(1);
+  }
+
+  return name;
+};
+
+export const validateForm = (values) => {
+  let errors = {};
+  if (!values.firstName) {
+    errors.firstName = 'Required';
+  } else if (!/^[A-Za-z- ]+$/i.test(values.firstName)) {
+    errors.firstName = 'Invalid format - Must only contain letters';
+  }
+  if (!values.lastName) {
+    errors.lastName = 'Required';
+  } else if (!/^[A-Za-z- ]+$/i.test(values.lastName)) {
+    errors.lastName = 'Invalid format - Must only contain letters';
+  }
+  if (!values.dateOfBirth) {
+    errors.dateOfBirth = 'Required';
+  }
+  if (!values.startDate) {
+    errors.startDate = 'Required';
+  }
+  if (!values.street) {
+    errors.street = 'Required';
+  }
+  if (!values.city) {
+    errors.city = 'Required';
+  }
+  if (!values.zipCode) {
+    errors.zipCode = 'Required';
+  }
+
+  return errors;
+};
--- src/utils/functions.test.js
+++ src/utils/functions.test.js
@@ -1,4 +1,4 @@
-import { sortList, filterList } from './functions';
+import { sortList, filterList, validateForm } from './functions';
 import { employeesList } from '../data/dataTest';
 
 describe('SortList function unit tests suite', () => {
@@ -159,5 +159,37 @@ describe('FilterList function unit tests suite', () => {
     //Case 3: filter key is a part of date or a number in text format
     const newList3 = filterList(employeesList, '1990');
     expect(newList3).toStrictEqual([employeesList[0]]);
+  });
+});
+
+describe('ValidateForm function tests suite', () => {
+  it('Should return an error for each field as required', () => {
+    const values = {
+      firstName: '',
+      lastName: '',
+      dateOfBirth: '',
+      startDate: '',
+      street: '',
+      city: '',
+      zipCode: '',
+    };
+    const errors = validateForm(values);
+    expect(errors.firstName).toBe('Required');
+    expect(errors.lastName).toBe('Required');
+    expect(errors.dateOfBirth).toBe('Required');
+    expect(errors.startDate).toBe('Required');
+    expect(errors.street).toBe('Required');
+    expect(errors.city).toBe('Required');
+    expect(errors.zipCode).toBe('Required');
+  });
+
+  it('Should return an error for each field as Invalid format - Must only contain letters', () => {
+    const values = {
+      firstName: '123',
+      lastName: '456',
+    };
+    const errors = validateForm(values);
+    expect(errors.firstName).toBe('Invalid format - Must only contain letters');
+    expect(errors.lastName).toBe('Invalid format - Must only contain letters');
   });
 });
