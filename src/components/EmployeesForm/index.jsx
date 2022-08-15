1da4bc571410933e664f656695807c1e8a947ae3
Patrice-H <webmestre64@gmail.com>
2022-08-16 00:35:09 +0200

    Outsourcing sub components

--- src/components/EmployeesForm/index.jsx
+++ src/components/EmployeesForm/index.jsx
@@ -1,10 +1,10 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
+import InputField from '../InputField';
 import DatePicker from '../DatePicker';
+import SelectMenu from '../SelectMenu';
 import { useAppDispatch } from '../../utils/hooks';
 import { addEmployee } from '../../redux/employeesSlice';
-import { formatDate } from '../../utils/functions';
-import { statesList } from '../../data/statesList';
-import { departmentsList } from '../../data/departmentsList';
+import { formatDate, validateForm } from '../../utils/functions';
 import './EmployeesForm.css';
 
 const initialValues = {
@@ -14,38 +14,9 @@ const initialValues = {
   startDate: '',
   street: '',
   city: '',
-  state: 'Alabama',
+  state: 'AL',
   zipCode: '',
   department: 'Sales',
-};
-
-const validate = (values) => {
-  let errors = {};
-  if (!values.firstName) {
-    errors.firstName = 'Required';
-  } else if (!/^[A-Za-z- ]+$/i.test(values.firstName)) {
-    errors.firstName = 'Invalid format - Must only contain letters';
-  }
-  if (!values.lastName) {
-    errors.lastName = 'Required';
-  }
-  if (!values.dateOfBirth) {
-    errors.dateOfBirth = 'Required';
-  }
-  if (!values.startDate) {
-    errors.startDate = 'Required';
-  }
-  if (!values.street) {
-    errors.street = 'Required';
-  }
-  if (!values.city) {
-    errors.city = 'Required';
-  }
-  if (!values.zipCode) {
-    errors.zipCode = 'Required';
-  }
-
-  return errors;
 };
 
 const EmployeesForm = () => {
@@ -55,7 +26,6 @@ const EmployeesForm = () => {
     const modal = document.getElementById('modal');
     values.dateOfBirth = formatDate(values.dateOfBirth);
     values.startDate = formatDate(values.startDate);
-    console.log('Form values : ', values);
     dispatch(addEmployee(values));
     modal.classList.remove('hidden');
   };
@@ -64,55 +34,21 @@ const EmployeesForm = () => {
     <Formik
       initialValues={initialValues}
       onSubmit={onSubmit}
-      validate={validate}
+      validate={validateForm}
     >
       <Form id="form">
-        <div className="form-control">
-          <label htmlFor="first-name">First Name</label>
-          <Field type="text" id="first-name" name="firstName" />
-          <ErrorMessage name="firstName" />
-        </div>
-        <div className="form-control">
-          <label htmlFor="last-name">Last Name</label>
-          <Field type="text" id="last-name" name="lastName" />
-          <ErrorMessage name="lastName" />
-        </div>
+        <InputField labelContent="First Name" inputType="text" />
+        <InputField labelContent="Last Name" inputType="text" />
         <DatePicker label="Date of Birth" name="dateOfBirth" />
         <DatePicker label="Start Date" name="startDate" />
         <fieldset>
           <legend>Address</legend>
-          <div className="form-control">
-            <label htmlFor="street">Street</label>
-            <Field type="text" id="street" name="street" />
-            <ErrorMessage name="street" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="city">City</label>
-            <Field type="text" id="city" name="city" />
-            <ErrorMessage name="city" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="state">State</label>
-            <Field name="state" as="select">
-              {statesList.map((state) => (
-                <option key={state.label}>{state.label}</option>
-              ))}
-            </Field>
-          </div>
-          <div className="form-control">
-            <label htmlFor="zip-code">Zip Code</label>
-            <Field type="number" id="zip-code" name="zipCode" />
-            <ErrorMessage name="zipCode" />
-          </div>
+          <InputField labelContent="Street" inputType="text" />
+          <InputField labelContent="City" inputType="text" />
+          <SelectMenu labelContent="State" />
+          <InputField labelContent="Zip Code" inputType="number" />
         </fieldset>
-        <div className="form-control">
-          <label htmlFor="department">Department</label>
-          <Field name="department" as="select" id="department">
-            {departmentsList.map((department) => (
-              <option key={department.label}>{department.label}</option>
-            ))}
-          </Field>
-        </div>
+        <SelectMenu labelContent="Department" />
         <div id="submit-btn">
           <button type="submit">Save</button>
         </div>
