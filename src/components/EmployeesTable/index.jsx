0d375bd2253af6661ac2efb0b8c22c8b20d08864
Patrice-H <webmestre64@gmail.com>
2022-08-20 06:34:00 +0200

    Embedding display options

--- src/components/EmployeesTable/index.jsx
+++ src/components/EmployeesTable/index.jsx
@@ -1,128 +1,167 @@
 import { useMemo } from 'react';
-import { useTable, useSortBy } from 'react-table';
+import {
+  useTable,
+  useSortBy,
+  useGlobalFilter,
+  usePagination,
+} from 'react-table';
 import { useAppSelector, useAppDispatch } from '../../utils/hooks';
 import { setSortBy, setOrderBy } from '../../redux/displayOptionsSlice';
 import { COLUMNS } from '../../data/table-columns';
-import { formatId } from '../../utils/functions';
-//import { applyUserOptions } from '../../utils/functions';
+import { formatId, formatName } from '../../utils/functions';
 import sortBtn from '../../assets/sort-btn.png';
 import sortAscBtn from '../../assets/sort-asc-btn.png';
 import sortDescBtn from '../../assets/sort-desc-btn.png';
+import HeaderOptions from '../HeaderOptions';
+import FooterOptions from '../FooterOptions';
 import './EmployeesTable.css';
 
 const EmployeesTable = () => {
   const dispatch = useAppDispatch();
   const employeesList = useAppSelector((state) => state.employees.list);
-  /*
   const sortedBy = useAppSelector((state) => state.displayOptions.sortBy);
   const orderedBy = useAppSelector((state) => state.displayOptions.orderBy);
+  const pageNumber = useAppSelector((state) => state.displayOptions.pageNumber);
+  const entries = useAppSelector((state) => state.displayOptions.tableLength);
+
   // prettier-ignore
   const filtredBy = useAppSelector((state) => state.displayOptions.searchFilter);
 
-  const displayedList = applyUserOptions(
-    employeesList,
-    filtredBy,
-    sortedBy,
-    orderedBy
-  );
-    */
   const columns = useMemo(() => COLUMNS, []);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const data = useMemo(() => employeesList, []);
-  //const data = useMemo(() => displayedList, []);
+  const data = useMemo(() => employeesList, [employeesList]);
+  const tableFilter = useMemo(() => filtredBy, [filtredBy]);
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(
-      {
-        columns,
-        data,
-      },
-      useSortBy
-    );
+  const sortBy =
+    sortedBy === null
+      ? []
+      : [{ id: formatName(sortedBy), desc: orderedBy === 'descending' }];
+
+  const initialState = {
+    sortBy,
+    globalFilter: tableFilter,
+    pageIndex: pageNumber - 1,
+    pageSize: entries,
+  };
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    prepareRow,
+    headerGroups,
+    rows,
+    page,
+    previousPage,
+    nextPage,
+    canPreviousPage,
+    canNextPage,
+    setPageSize,
+    setGlobalFilter,
+    state,
+  } = useTable(
+    {
+      initialState,
+      columns,
+      data,
+    },
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
+  const { pageSize } = state;
 
   const updateDisplayOptions = (column) => {
-    let sort, order;
     if (!column.isSorted && column.isSortedDesc === undefined) {
-      sort = formatId(column.Header);
-      order = 'ascending';
+      dispatch(setSortBy(formatId(column.Header)));
+      if (orderedBy !== 'ascending') {
+        dispatch(setOrderBy('ascending'));
+      }
     }
     if (column.isSorted && !column.isSortedDesc) {
-      sort = formatId(column.Header);
-      order = 'descending';
+      dispatch(setOrderBy('descending'));
     }
     if (column.isSorted && column.isSortedDesc) {
-      sort = null;
-      order = null;
+      dispatch(setSortBy(null));
+      dispatch(setOrderBy(null));
     }
-    console.log('sort ' + sort + ' order ' + order);
-    dispatch(setSortBy(sort));
-    dispatch(setOrderBy(order));
   };
 
   return (
-    <div id="employees-table" data-testid="employees-table">
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column, index) => (
-                <th
-                  key={index}
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                >
-                  <div
-                    className="th"
-                    onClick={() => {
-                      updateDisplayOptions(column);
-                    }}
+    <>
+      <HeaderOptions
+        pageSize={pageSize}
+        setPageSize={setPageSize}
+        setFilter={setGlobalFilter}
+      />
+      <div id="employees-table" data-testid="employees-table">
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column, index) => (
+                  <th
+                    key={index}
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
                   >
-                    <span className="header-column">
-                      {column.render('Header')}
-                    </span>
-                    <span className="sort-icon">
-                      <img
-                        src={
-                          column.isSorted
-                            ? column.isSortedDesc
-                              ? sortDescBtn
-                              : sortAscBtn
-                            : sortBtn
-                        }
-                        alt="Sort Button"
-                      />
-                    </span>
-                  </div>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        {employeesList.length === 0 ? (
-          <tbody>
-            <tr>
-              <td colSpan={9} id="no-data">
-                No data available in table
-              </td>
-            </tr>
-          </tbody>
-        ) : (
+                    <div
+                      className="th"
+                      onClick={() => {
+                        updateDisplayOptions(column);
+                      }}
+                    >
+                      <span className="header-column">
+                        {column.render('Header')}
+                      </span>
+                      <span className="sort-icon">
+                        <img
+                          src={
+                            column.isSorted
+                              ? column.isSortedDesc
+                                ? sortDescBtn
+                                : sortAscBtn
+                              : sortBtn
+                          }
+                          alt="Sort Button"
+                        />
+                      </span>
+                    </div>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
           <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
+            {page.length > 0 ? (
+              page.map((row) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => {
+                      return (
+                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                      );
+                    })}
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan={9} id="no-data">
+                  No data available in table
+                </td>
+              </tr>
+            )}
           </tbody>
-        )}
-      </table>
-    </div>
+        </table>
+      </div>
+      <FooterOptions
+        total={rows.length}
+        previousPage={previousPage}
+        nextPage={nextPage}
+        canPreviousPage={canPreviousPage}
+        canNextPage={canNextPage}
+      />
+    </>
   );
 };
 
