249eaa2206ace56d1bb6dc5e6854690ca56c9de2
Patrice-H <webmestre64@gmail.com>
2022-08-26 14:28:01 +0200

    Adding style for sorted column

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -156,7 +156,16 @@ const EmployeesTable = () => {
                   <tr {...row.getRowProps()}>
                     {row.cells.map((cell) => {
                       return (
-                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                        <td
+                          {...cell.getCellProps()}
+                          className={
+                            formatId(cell.column.Header) === sortedBy
+                              ? 'sorted-column'
+                              : null
+                          }
+                        >
+                          {cell.render('Cell')}
+                        </td>
                       );
                     })}
                   </tr>
